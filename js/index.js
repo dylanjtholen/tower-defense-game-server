df26e18275ad49c5b409607beea34f6d1fa89bfd
Dylanjtholen <100318188+heeheehehaw@users.noreply.github.com>
2022-11-15 19:56:46 +0000

    a

--- js/index.js
+++ js/index.js
@@ -1,22 +1,20 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width = 960
-canvas.height = 640
+canvas.width = 960;
+canvas.height = 640;
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillStyle = 'white';
+c.fillRect(0, 0, canvas.width, canvas.height);
 
-var map = new Image()
-map.src = "assets/img/map.png"
-map.onload = () => {
-
-}
+var map = new Image();
+map.src = "assets/img/map.png";
 window.addEventListener('load', () => {
     window.requestAnimationFrame(mainloop);
 });
 
 function mainloop() {
-    c.drawImage(map, 0, 0, canvas.width, canvas.height)
-    window.requestAnimationFrame(mainloop)
-}
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.drawImage(map, 0, 0, canvas.width, canvas.height);
+    window.requestAnimationFrame(mainloop);
+};
